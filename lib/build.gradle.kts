873076c0986db1167ac8b9cfb4379bbdfcbe098d
Danylo Andrieiev <danandryeyev@gmail.com>
2025-01-08 14:19:54 +0200

    First publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,9 +1,14 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.vanniktech.maven.publish) apply false
-    alias(libs.plugins.gradleup.nmcp) apply false
+    id("maven-publish")
 }
+
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
 
 android {
     namespace = "com.danitze.scanfusionlib"
@@ -34,6 +39,70 @@ android {
     }
     buildFeatures {
         viewBinding = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            groupId = project.findProperty("GROUP")?.toString()
+            artifactId = project.findProperty("POM_ARTIFACT_ID")?.toString()
+            version = project.findProperty("VERSION_NAME")?.toString()
+            artifact("$buildDir/outputs/aar/lib-release.aar")
+
+            val pomName = project.findProperty("POM_NAME")?.toString()
+            val pomDescription = project.findProperty("POM_DESCRIPTION")?.toString()
+            val pomUrl = project.findProperty("POM_URL")?.toString()
+            val licenseName = project.findProperty("POM_LICENSE_NAME")?.toString()
+            val licenseUrl = project.findProperty("POM_LICENSE_URL")?.toString()
+            val developerId = project.findProperty("POM_DEVELOPER_ID")?.toString()
+            val developerName = project.findProperty("POM_DEVELOPER_NAME")?.toString()
+            val developerUrl = project.findProperty("POM_DEVELOPER_URL")?.toString()
+
+            pom {
+                name.set(pomName)
+                description.set(pomDescription)
+                url.set(pomUrl)
+
+                licenses {
+                    license {
+                        name.set(licenseName)
+                        url.set(licenseUrl)
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set(developerId)
+                        name.set(developerName)
+                        url.set(developerUrl)
+                    }
+                }
+
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/danitze/ScanFusion/issues")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/danitze/ScanFusion")
+
+            credentials {
+                username = githubProperties["gpr.usr"]?.toString() ?: System.getenv("GPR_USER")
+                password = githubProperties["gpr.key"]?.toString() ?: System.getenv("GPR_API_KEY")
+            }
+        }
     }
 }
 
